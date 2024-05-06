54c198d4898a907045e9c6cd2fbb8c92a9ce92f2
Arafat Khan Pathan <163514145+pAthAn13913@users.noreply.github.com>
2024-05-06 17:44:53 +0600

    Update Ans__02__A.java

--- Lab__Final/Ans__02__A.java
+++ Lab__Final/Ans__02__A.java
@@ -29,3 +29,36 @@ public class InFileNumber {
         }
     }
 }
+
+// Another type
+
+// import java.io.File;
+// import java.io.FileWriter;
+// import java.util.Scanner;
+
+// public class Forexample{
+//     public static void main(String[] args) {
+//         try {
+//             FileWriter myWrite = new FileWriter("src//b.txt");
+//             Scanner sc = new Scanner(new File("src//a.txt"));
+
+//             String line1 = sc.nextLine();
+//             char ch []= new char[20];
+
+//             String result = "";
+
+//             for (int i = 0; i < line1.length(); i++) {
+//                 char c = line1.charAt(i);
+//                 ch[i]=c;
+//             }
+
+//             result=ch[4]+""+ch[5]+""+ch[9]+""+ch[13]+"";
+//             myWrite.write(result);
+//             myWrite.close();
+//             sc.close();
+
+//         } catch (Exception e) {
+//             System.err.println("WRONG");
+//         }
+//     }
+// }
