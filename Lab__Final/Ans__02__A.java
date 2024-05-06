f30f69bfde6bb792a6bfee71df897235424fbc12
Arafat Khan Pathan <163514145+pAthAn13913@users.noreply.github.com>
2024-05-06 17:19:52 +0600

    Create Ans__02__A.java

---
+++ Lab__Final/Ans__02__A.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class InFileNumber {
+    public static void main(String[] args) {
+        try {
+            FileWriter myWrite = new FileWriter("src//b.txt");
+            Scanner sc = new Scanner(new File("src//a.txt"));
+
+            String line1 = sc.nextLine();
+
+            String result = "";
+
+            for (int i = 0; i < line1.length(); i++) {
+                char c = line1.charAt(i);
+                if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' ||
+                    c == '5' || c == '6' || c == '7' || c == '8' || c == '9' ) {
+
+                    result += c;
+                }
+            }
+            myWrite.write(result);
+            myWrite.close();
+            sc.close();
+
+        } catch (Exception e) {
+            System.err.println("WRONG");
+        }
+    }
+}
