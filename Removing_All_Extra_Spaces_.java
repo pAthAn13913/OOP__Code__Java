52ce42e0d578f893d260da9a31fb643aaf67c119
Arafat Khan Pathan <163514145+pAthAn13913@users.noreply.github.com>
2024-05-09 19:01:16 +0600

    Update Removing_All_Extra_Spaces_.java

--- Removing_All_Extra_Spaces_.java
+++ Removing_All_Extra_Spaces_.java
@@ -42,3 +42,54 @@ public class Remove {
     }
 
 }
+
+////// removeing all dublicate character
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Dublicate_Remove {
+    public static void main(String[] args) {
+
+        System.out.println("2nd");
+        try {
+            Scanner sc = new Scanner(new File("src/Input.txt"));
+            FileWriter w = new FileWriter("src/Output.txt");
+
+            String line1 = sc.nextLine();
+            removeDuplicate(line1);
+
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+    }
+
+    public static void removeDuplicate(String line) {
+
+        int i, j, k, x;
+        int len = line.length();
+        char ch[] = new char[len];
+
+        for (i = 0; i < len; i++) {
+            ch[i] = line.charAt(i);
+
+        }
+
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; ) {
+                if (ch[i] == ch[j]) {
+                    for (k = j; k < len - 1; k++) {
+                        ch[k] = ch[k + 1];
+                    }
+                    len--;
+                } else j++;
+            }
+        }
+        for (i = 0; i < len; i++) {
+            System.out.print(ch[i]);
+        }
+    }
+
+}
+
